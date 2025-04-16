4bad614a65312c3f650c23a2170c4917fb21f3e1
Marc Munene <marcmunene104@gmail.com>
2025-04-16 20:07:22 +0300

    public

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <header>
+          <div>
+            <img src="/public/munene.dev2.png"  alt="Logo" />
+          </div>
+        </header>
+      </div>
+    </div>
+  );
 };
 
 export { App };
