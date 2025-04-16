60ff03d5e18ab0f8474fd480961cc50e5806e183
Marc Munene <marcmunene104@gmail.com>
2025-04-16 20:17:53 +0300

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { BsInstagram } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+
 const App = () => {
   return (
     <div>
       <div>
         <header>
-          <div>
-            <img src="/public/munene.dev2.png"  alt="Logo" />
-          </div>
+          <nav>
+            <div>
+              <img src="/public/munene.dev2.png" alt="Logo" />
+            </div>
+            <div>
+              <ul>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Profession</li>
+              </ul>
+            </div>
+            <div>
+              <FaGithub />
+              <BsInstagram />
+              <IoIosMail />
+            </div>
+          </nav>
         </header>
       </div>
     </div>
