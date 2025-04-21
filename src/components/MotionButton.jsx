deb2e72ea2110b0e809764146bb3fc959f6ab45d
Marc Munene <marcmunene104@gmail.com>
2025-04-21 10:10:20 +0300

    motion

--- src/components/MotionButton.jsx
+++ src/components/MotionButton.jsx
@@ -8,7 +8,7 @@ const MotionButton = () => {
       <motion.button
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.56 }}
-        onHoverStart={() => console.log("hover started!")}
+        // onHoverStart={() => console.log("hover started!")}
         className="border border-cyan-500 text-cyan-500 hover:bg-cyan-500/10 px-4 py-2 sm:px-6 sm:py-3 rounded-lg cursor-pointer transition-all duration-300"
       >
         contact Me?
